57406b80360b38e94886caa68f44ddf1c90980f4
MounaAlkaabi <m.s.alkaabi90@hotmail.com>
2016-10-12 09:40:24 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from Bicycle import *
+
+if __name__ == '__main__':
+
+    """Create 6 different bicycle models"""
+    model_one = Bicycle("Speedy", 20, 200)
+    model_two= Bicycle("Blazer", 40, 300)
+    model_three = Bicycle("Hip", 35, 650)
+    model_four = Bicycle("Downhill", 20, 720)
+    model_five = Bicycle("Hop", 45, 475)
+    model_six = Bicycle("Skip", 30, 250)
